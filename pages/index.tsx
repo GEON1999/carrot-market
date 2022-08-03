85114eec132754f41fe4830df4fa57789b38efd7
GEON1999 <phgst12@gmail.com>
2022-08-03 12:13:35 +0900

    Tailwind CSS setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
-  return null;
+  return (
+    <div className="bg-red-500">
+      <h1 className="text-blue-500">hi nextJs</h1>
+    </div>
+  );
 };
 
 export default Home;
