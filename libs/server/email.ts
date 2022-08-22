27dab35b390f5e66061634a4669cd6be7b401c7e
GEON1999 <phgst12@gmail.com>
2022-08-22 13:44:57 +0900

    email authentication with 'nodemailer'

---
+++ libs/server/email.ts
@@ -0,0 +1,16 @@
+import nodemailer from "nodemailer";
+
+const smtpTransport = nodemailer.createTransport({
+  service: "Naver",
+  host: "smtp.naver.com",
+  port: 587,
+  auth: {
+    user: process.env.MAIL_ID,
+    pass: process.env.MAIL_PASSWORD,
+  },
+  tls: {
+    rejectUnauthorized: false,
+  },
+});
+
+export default smtpTransport;
--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,6 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next";
 import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import twilio from "twilio";
+import smtpTransport from "@libs/server/email";
 
 const twilioClient = twilio(process.env.TWILIO_SID, process.env.TWILIO_TOKEN);
 
@@ -15,6 +16,7 @@ async function handler(
     return res.status(400).json({ ok: false });
   }
   const payload = Math.floor(100000 + Math.random() * 9000000) + "";
+
   const token = await client.token.create({
     data: {
       token: payload,
@@ -38,6 +40,27 @@ async function handler(
       body: `your token is ${payload}`,
     });
     console.log(message);
+  } else if (email) {
+    const mailOptions = {
+      from: process.env.MAIL_ID,
+      to: email,
+      subject: "Verification Code From Carrot Market",
+      text: `Verification Code : ${payload}`,
+    };
+    const result = await smtpTransport.sendMail(
+      mailOptions,
+      (error, responses) => {
+        if (error) {
+          console.log(error);
+          return null;
+        } else {
+          console.log(responses);
+          return null;
+        }
+      }
+    );
+    smtpTransport.close();
+    console.log(result);
   }
   console.log(token);
   return res.json({
