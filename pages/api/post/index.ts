aa8fac4e848f876854324b47d99f79eb40e85af6
GEON1999 <phgst12@gmail.com>
2022-08-29 17:16:07 +0900

    Post index

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -8,21 +8,46 @@ async function handler(req: NextApiRequest, res: NextApiResponse) {
     body: { question },
     session: { user },
   } = req;
-
-  const post = await client.post.create({
-    data: {
-      question,
-      user: {
-        connect: {
-          id: user?.id,
+  if (req.method === "POST") {
+    const post = await client.post.create({
+      data: {
+        question,
+        user: {
+          connect: {
+            id: user?.id,
+          },
         },
       },
-    },
-  });
-  res.json({
-    ok: true,
-    post,
-  });
+    });
+    res.json({
+      ok: true,
+      post,
+    });
+  }
+  if (req.method === "GET") {
+    const posts = await client.post.findMany({
+      include: {
+        user: {
+          select: {
+            id: true,
+            name: true,
+          },
+        },
+        _count: {
+          select: {
+            interest: true,
+            comments: true,
+          },
+        },
+      },
+    });
+    res.json({
+      ok: true,
+      posts,
+    });
+  }
 }
 
-export default withApiSession(withHandler({ methods: ["POST"], handler }));
+export default withApiSession(
+  withHandler({ methods: ["POST", "GET"], handler })
+);
--- pages/community/index.tsx
+++ pages/community/index.tsx
@@ -2,66 +2,91 @@ import type { NextPage } from "next";
 import Link from "next/link";
 import FixedBtn from "@components/fixedBtn";
 import Layout from "@components/layout";
+import useSWR from "swr";
+import { Post, User } from "@prisma/client";
+import useMutation from "@libs/client/useMutation";
+import { useRouter } from "next/router";
+import { useForm } from "react-hook-form";
+
+interface PostwithUser extends Post {
+  user: User;
+  _count: {
+    interest: number;
+    comments: number;
+  };
+}
+
+interface PostsResponse {
+  ok: boolean;
+  posts: PostwithUser[];
+}
 
 const Community: NextPage = () => {
+  const { data } = useSWR<PostsResponse>(`/api/post`);
   return (
     <Layout title="동네생활" hasTabBar>
       <div className="mx-4 py-2">
-        {[1, 2, 3, 4, 5, 6].map((i) => (
-          <Link href="community/id" key={i}>
-            <a>
-              <div className="flex flex-col items-start my-5">
-                <span className="bg-gray-100 text-gray-700 px-2.5 py-0.5 rounded-full text-sm  mb-2">
-                  동네질문
-                </span>
-                <span>
-                  <span className="text-orange-500 font-bold">Q.</span> What is
-                  the best mandu restaurant?
-                </span>
-                <div className="my-4 flex justify-between w-full text-gray-500 text-sm">
-                  <span>니꼬</span>
-                  <span>18시간 전</span>
-                </div>
-                <div className="flex space-x-3 border-t border-b-[2px] w-full py-2 text-sm text-gray-700">
-                  <span className="flex items-center space-x-1">
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
-                      ></path>
-                    </svg>
-                    <span>궁금해요 1</span>
+        {data?.posts?.map((post) => (
+          <div key={post.id}>
+            <Link href={`/community/${post.id}`}>
+              <a>
+                <div className="flex flex-col items-start my-5">
+                  <span className="bg-gray-100 text-gray-700 px-2.5 py-0.5 rounded-full text-sm  mb-2">
+                    동네질문
                   </span>
-                  <span className="flex items-center space-x-1">
-                    <svg
-                      className="w-4 h-4"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
-                      ></path>
-                    </svg>
-                    <span>답변 1</span>
+                  <span>
+                    <span className="text-orange-500 font-bold">Q.</span>
+                    {post.question}
                   </span>
+                  <div className="my-4 flex justify-between w-full text-gray-500 text-sm">
+                    <span>{post.user.name}</span>
+                    <span>{String(post.createdAt)}</span>
+                  </div>
                 </div>
-              </div>
-            </a>
-          </Link>
+              </a>
+            </Link>
+            <div className="flex space-x-3 border-t border-b-[2px] w-full py-2 text-sm text-gray-700">
+              <button>
+                <span className="flex items-center space-x-1">
+                  <svg
+                    className="w-4 h-4"
+                    fill="none"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="2"
+                      d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                    ></path>
+                  </svg>
+                  <span>궁금해요 {post._count.interest}</span>
+                </span>
+              </button>
+
+              <span className="flex items-center space-x-1">
+                <svg
+                  className="w-4 h-4"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+                  ></path>
+                </svg>
+                <span>답변 {post._count.comments}</span>
+              </span>
+            </div>
+          </div>
         ))}
+
         <Link href="community/write">
           <a>
             <FixedBtn>
