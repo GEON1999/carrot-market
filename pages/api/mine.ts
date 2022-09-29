153977dea43cbfd4de71d636bae0a6fa6dc6916b
GEON1999 <phgst12@gmail.com>
2022-09-29 14:21:50 +0900

    Profile home (products & post)

---
+++ pages/api/mine.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { createDeflate } from "zlib";
+
+async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const {
+    session: { user },
+  } = req;
+  const products = await client.product.findMany({
+    where: {
+      user: {
+        id: user?.id,
+      },
+    },
+    include: {
+      reviews: {
+        select: {
+          id: true,
+        },
+      },
+    },
+  });
+  const post = await client.post.findMany({
+    where: {
+      user: {
+        id: user?.id,
+      },
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
+      _count: {
+        select: {
+          interest: true,
+          comments: true,
+        },
+      },
+    },
+  });
+  res.json({ ok: true, products, post });
+}
+
+export default withApiSession(withHandler({ methods: ["GET"], handler }));
--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -6,8 +6,9 @@ import useSWR from "swr";
 import { Review, User } from "@prisma/client";
 import useSWRInfinite from "swr/infinite";
 import useScrollpage from "@libs/client/scrollPage";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import timeForToday from "@libs/client/timeForToday";
+import Item from "@components/item";
 
 interface ReviewWithUser extends Review {
   leavedBy: User;
@@ -28,11 +29,15 @@ const getKey = (pageIndex: number) => {
 
 const Profile: NextPage = () => {
   const { data: userData } = useSWR<UserProfile>("/api/users/me");
+  const { data: mineData } = useSWR("/api/mine");
+  console.log(mineData);
   const user = userData?.profile;
   const { data, setSize } = useSWRInfinite<ReviewsResponse>(getKey, {
     initialSize: 1,
     revalidateAll: false,
   });
+  const [post, setPost] = useState(false);
+  const [product, setProduct] = useState(false);
   const reviews = data?.map((i) => i.reviews).flat();
   const page = useScrollpage();
   useEffect(() => {
@@ -54,8 +59,8 @@ const Profile: NextPage = () => {
         <div className="flex justify-between px-5 my-12">
           <Link href="profile/sold">
             <a>
-              <div className="text-sm">
-                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600">
+              <div className="flex flex-col justify-center text-sm">
+                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600 shadow-sm  flex-col self-center">
                   <svg
                     className="w-6 h-6"
                     fill="none"
@@ -71,14 +76,17 @@ const Profile: NextPage = () => {
                     ></path>
                   </svg>
                 </div>
-                <span>판매내역</span>
+                <span className="px-2 py-1 bg-slate-100 rounded-lg">
+                  판매내역
+                </span>
               </div>
             </a>
           </Link>
+
           <Link href="profile/bought">
             <a>
               <div className="flex flex-col justify-center text-sm">
-                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600">
+                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600 shadow-sm  flex-col self-center">
                   <svg
                     className="w-6 h-6"
                     fill="none"
@@ -94,14 +102,16 @@ const Profile: NextPage = () => {
                     ></path>
                   </svg>
                 </div>
-                <span>구매내역</span>
+                <span className="px-2 py-1 bg-slate-100 rounded-lg">
+                  구매내역
+                </span>
               </div>
             </a>
           </Link>
           <Link href="profile/loved">
             <a>
               <div className="flex flex-col justify-center text-sm">
-                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600">
+                <div className="w-14 h-14 rounded-full bg-orange-500 flex justify-center items-center text-white mb-2 hover:bg-orange-600 shadow-sm  flex-col self-center">
                   <svg
                     className="w-6 h-6"
                     fill="none"
@@ -117,34 +127,121 @@ const Profile: NextPage = () => {
                     ></path>
                   </svg>
                 </div>
-                <span>관심목록</span>
+                <span className="px-2 py-1 bg-slate-100 rounded-lg">
+                  관심목록
+                </span>
               </div>
             </a>
           </Link>
         </div>
-        <>
-          <h1 className="mb-7 text-sm font-bold">받은 거래 후기</h1>
-          {reviews?.map((review) => (
-            <div key={review?.id} className="space-y-6 mt-4">
-              <ProfileInfo
-                big={false}
-                name={review?.leavedBy.name}
-                subtitle={timeForToday(review?.createdAt)}
-                avatar={review?.leavedBy.avatar}
+        <div className="mb-16">
+          <div className="flex pb-2 border-b w-32">
+            <button
+              onClick={() => {
+                setPost((prev) => !prev);
+              }}
+              className=" text-sm font-bold pr-1"
+            >
+              나의 동네생활
+            </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              strokeWidth={2}
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19 20H5a2 2 0 01-2-2V6a2 2 0 012-2h10a2 2 0 012 2v1m2 13a2 2 0 01-2-2V7m2 13a2 2 0 002-2V9a2 2 0 00-2-2h-2m-4-3H9M7 16h6M7 8h6v4H7V8z"
               />
-              <div className="border-b border-gray-100">
-                <p className="text-gray-700 text-sm  -mt-5 ml-12 mb-4">
-                  {review?.review}
-                </p>
-              </div>
+            </svg>
+          </div>
+          {post
+            ? mineData?.post?.map((post: any) => (
+                <div key={post?.id} className="border-b">
+                  <Link href={`/community/${post?.id}`}>
+                    <a>
+                      <div className="flex flex-col items-start my-4">
+                        <span className="bg-gray-100 text-gray-700 px-2.5 py-0.5 rounded-full text-sm  mb-2">
+                          동네질문
+                        </span>
+
+                        <div className="my-2 flex justify-between w-full text-gray-500 text-sm">
+                          <span>
+                            <span className="text-orange-500 font-bold">
+                              Q.{" "}
+                            </span>
+                            {post?.question}
+                          </span>
+                          <span>{timeForToday(post?.createdAt)}</span>
+                        </div>
+                      </div>
+                    </a>
+                  </Link>
+                </div>
+              ))
+            : null}
+        </div>
+        <div>
+          <div className="flex pb-2 border-b w-24">
+            <button
+              onClick={() => setProduct((prev) => !prev)}
+              className=" text-sm font-bold  pr-1"
+            >
+              나의 상품
+            </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-6 h-6 "
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M13.5 21v-7.5a.75.75 0 01.75-.75h3a.75.75 0 01.75.75V21m-4.5 0H2.36m11.14 0H18m0 0h3.64m-1.39 0V9.349m-16.5 11.65V9.35m0 0a3.001 3.001 0 003.75-.615A2.993 2.993 0 009.75 9.75c.896 0 1.7-.393 2.25-1.016a2.993 2.993 0 002.25 1.016c.896 0 1.7-.393 2.25-1.016a3.001 3.001 0 003.75.614m-16.5 0a3.004 3.004 0 01-.621-4.72L4.318 3.44A1.5 1.5 0 015.378 3h13.243a1.5 1.5 0 011.06.44l1.19 1.189a3 3 0 01-.621 4.72m-13.5 8.65h3.75a.75.75 0 00.75-.75V13.5a.75.75 0 00-.75-.75H6.75a.75.75 0 00-.75.75v3.75c0 .415.336.75.75.75z"
+              />
+            </svg>
+          </div>
+          {product
+            ? mineData?.products?.map((product: any) => (
+                <div key={product?.id}>
+                  {product?.reviews[0]?.id ? null : (
+                    <Item
+                      id={product?.id}
+                      title={product?.title}
+                      subtitle={`${product?.subTitle} · ${timeForToday(
+                        Number(new Date(product?.createdAt))
+                      )}`}
+                      price={product?.price}
+                      prodcut={product?.image ? product.image : null}
+                    />
+                  )}
+                </div>
+              ))
+            : null}
+        </div>
+        <h2 className="mt-16 mb-8 text-sm font-bold ">받은 거래 후기</h2>
+        {reviews?.map((review) => (
+          <div key={review?.id} className="space-y-6 mt-4">
+            <ProfileInfo
+              big={false}
+              name={review?.leavedBy.name}
+              subtitle={timeForToday(review?.createdAt)}
+              avatar={review?.leavedBy.avatar}
+            />
+            <div className="border-b border-gray-100">
+              <p className="text-gray-700 text-sm  -mt-5 ml-12 mb-4">
+                {review?.review}
+              </p>
             </div>
-          ))}
-          {page >= 2 ? (
-            <div className="p-10 text-center text-xl text-gray-500">
-              no more content
-            </div>
-          ) : null}
-        </>
+          </div>
+        ))}
       </div>
     </Layout>
   );
