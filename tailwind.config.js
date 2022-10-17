512b883ae897c4db5de32fd4e180eba4a59dd6dc
GEON1999 <phgst12@gmail.com>
2022-10-17 20:31:41 +0900

    responsive web set up

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,22 @@ module.exports = {
     "./components/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
+    screens: {
+      sm: { max: "767px" },
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      nav: { min: "768px", max: "1100px" },
+      // => @media (min-width: 1024px) { ... }
+
+      xl: "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "2xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       keyframes: {
         wiggle: {
