59904f0ce9b53c32d55ef97561f084ec1d79ae3f
GEON1999 <phgst12@gmail.com>
2022-10-19 03:17:51 +0900

    fix Hydration issue

--- pages/chats/index.tsx
+++ pages/chats/index.tsx
@@ -7,6 +7,7 @@ import { ChatRoom, Messages, User } from "@prisma/client";
 import { useState } from "react";
 import timeForToday from "@libs/client/timeForToday";
 import useDelete from "@libs/client/useDelete";
+import { useRouter } from "next/router";
 
 interface ChatRoomWith extends ChatRoom {
   messages: Messages[];
@@ -20,6 +21,7 @@ interface ChatRoomResponse {
   chatRooms: ChatRoomWith[];
 }
 const Chats: NextPage = () => {
+  const router = useRouter();
   const { data: userData } = useSWR("/api/users/me");
   const { data } = useSWR<ChatRoomResponse>(`/api/chatRoom`);
   const [active, setActive] = useState(false);
@@ -55,80 +57,77 @@ const Chats: NextPage = () => {
         </button>
       </div>
       <div className="divide-y-[1px] sm:py-2 md:max-w-4xl md:mx-auto">
-        {data?.chatRooms?.map((chatRoom: any) => (
-          <Link href={active ? "" : `chats/${chatRoom.id}`} key={chatRoom.id}>
-            <a>
-              {chatRoom.sellerId === userData?.profile?.id ||
-              chatRoom.buyerId === userData?.profile?.id ? (
-                <>
-                  {chatRoom.messages[0] ? (
-                    <div className="flex justify-between items-center">
-                      <ProfileInfo
-                        big
-                        name={
-                          chatRoom.sellerId === userData?.profile?.id
-                            ? chatRoom.buyer.name
-                            : chatRoom.buyerId === userData?.profile?.id
-                            ? chatRoom.seller.name
-                            : ""
-                        }
-                        avatar={
-                          chatRoom.sellerId === userData?.profile?.id
-                            ? chatRoom.buyer.avatar
-                            : chatRoom.buyerId === userData?.profile?.id
-                            ? chatRoom.seller.avatar
-                            : ""
-                        }
-                        subtitle={chatRoom.messages?.[0].message}
-                        position={"m-4"}
-                      />
-                      <div className="mr-4 space-x-2 flex items-center">
-                        {chatRoom.messages?.[0].userId !==
-                        userData?.profile?.id ? (
-                          chatRoom._count.notifications !== 0 ? (
-                            <span className="text-sm text-white w-5 h-5 bg-orange-400 rounded-full flex items-center justify-center">
-                              {chatRoom._count.notifications}
-                            </span>
-                          ) : (
-                            <span></span>
-                          )
-                        ) : (
-                          <span></span>
-                        )}
-                        {active ? (
-                          <button onClick={() => onClicked(chatRoom.id)}>
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth={1.5}
-                              stroke="currentColor"
-                              className="w-6 h-6 text-gray-700 dark:text-gray-300"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
-                              />
-                            </svg>
-                          </button>
-                        ) : (
-                          <span className="text-xs text-gray-400 mr-5 ">
-                            {timeForToday(
-                              Number(
-                                new Date(`${chatRoom.messages[0].updatedAt}`)
-                              )
-                            )}
-                          </span>
-                        )}
-                      </div>
-                    </div>
-                  ) : null}
-                </>
-              ) : null}
-            </a>
-          </Link>
-        ))}
+        {data?.chatRooms?.map((chatRoom: any) =>
+          chatRoom.sellerId === userData?.profile?.id ||
+          chatRoom.buyerId === userData?.profile?.id ? (
+            chatRoom.messages[0] ? (
+              <div
+                className="flex justify-between items-center border-none"
+                key={chatRoom.id}
+                onClick={() =>
+                  router.push(active ? "" : `chats/${chatRoom.id}`)
+                }
+              >
+                <ProfileInfo
+                  big
+                  name={
+                    chatRoom.sellerId === userData?.profile?.id
+                      ? chatRoom.buyer.name
+                      : chatRoom.buyerId === userData?.profile?.id
+                      ? chatRoom.seller.name
+                      : ""
+                  }
+                  avatar={
+                    chatRoom.sellerId === userData?.profile?.id
+                      ? chatRoom.buyer.avatar
+                      : chatRoom.buyerId === userData?.profile?.id
+                      ? chatRoom.seller.avatar
+                      : ""
+                  }
+                  subtitle={chatRoom.messages?.[0].message}
+                  position={"m-4"}
+                />
+                <div className="mr-4 space-x-2 flex items-center">
+                  {chatRoom.messages?.[0].userId !== userData?.profile?.id ? (
+                    chatRoom._count.notifications !== 0 ? (
+                      <span className="text-sm text-white w-5 h-5 bg-orange-400 rounded-full flex items-center justify-center">
+                        {chatRoom._count.notifications}
+                      </span>
+                    ) : (
+                      <span></span>
+                    )
+                  ) : (
+                    <span></span>
+                  )}
+                  {active ? (
+                    <button onClick={() => onClicked(chatRoom.id)}>
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        strokeWidth={1.5}
+                        stroke="currentColor"
+                        className="w-6 h-6 text-gray-700 dark:text-gray-300"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          d="M15 12H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z"
+                        />
+                      </svg>
+                    </button>
+                  ) : (
+                    <span className="text-xs text-gray-400 mr-5 ">
+                      {timeForToday(
+                        Number(new Date(`${chatRoom.messages[0].updatedAt}`))
+                      )}
+                    </span>
+                  )}
+                </div>
+              </div>
+            ) : null
+          ) : null
+        )}
       </div>
     </Layout>
   );
