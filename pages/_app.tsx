5e051f831416009cb55f58351ae72ae30079c1dc
GEON1999 <phgst12@gmail.com>
2022-10-19 02:35:43 +0900

    Dark mode : Set Up

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SWRConfig } from "swr";
 import useUser from "@libs/client/useUser";
+import { useState } from "react";
+import { useRouter } from "next/router";
 
 function ValidateUser() {
   useUser();
@@ -9,15 +11,58 @@ function ValidateUser() {
 }
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [darkMode, setDarkMode] = useState(false);
+  const router = useRouter();
+
   return (
     <SWRConfig
       value={{
         fetcher: (url: string) => fetch(url).then((res) => res.json()),
       }}
     >
-      <div className="w-full max-w-lg md:max-w-full mx-auto md:px-10 ">
-        <ValidateUser />
-        <Component {...pageProps} />
+      <div className={`${darkMode === true ? `dark` : `light`}`}>
+        <div className="w-full max-w-lg md:max-w-full mx-auto md:px-10 h-screen dark:bg-slate-800 dark:text-white dark:border-gray-400">
+          {router.pathname === "/profile" ? (
+            <button
+              className="fixed top-2.5 right-7 p-1  z-20 md:top-5 md:right-36"
+              onClick={() => setDarkMode((prev) => !prev)}
+            >
+              {darkMode === true ? (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 md:w-8 md:h-8"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+                  />
+                </svg>
+              ) : (
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  strokeWidth={1.5}
+                  stroke="currentColor"
+                  className="w-6 h-6 md:w-7 md:h-7"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
+                  />
+                </svg>
+              )}
+            </button>
+          ) : null}
+          <ValidateUser />
+          <Component {...pageProps} />
+        </div>
       </div>
     </SWRConfig>
   );
